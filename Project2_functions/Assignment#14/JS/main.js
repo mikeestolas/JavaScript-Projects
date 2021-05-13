f3a560c08f8e28b7e1e8c0b3bc72259ad05ff0da
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-05-13 18:02:46 -0400

    .

--- Project2_functions/Assignment#14/JS/main.js
+++ Project2_functions/Assignment#14/JS/main.js
@@ -1,6 +1,8 @@
-function myfunction() {
-var sentence = "I am learning";
+function myfunction() {                             //Defining a function and naming itv
+var sentence = "I am learning";                     //Deining a variable and gving it a
+var str = "This text is Green!";
+var result = str.fontcolor("green");                //using the fontcolor method on      
 sentence += "a lot from this book!";
-document.getElementById("concatenate").innerHTML =
-sentence;
-}
+document.getElementById("concatenate").innerHTML = sentence + "<br>" + result;   //Putting the value
+                                                               //of result into HTML element with "concatenate id
+}                                                              

b39e16483a66be62ab7ba5adaec0032064dac514
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-05-13 21:52:01 -0400

    .

--- Assigment_submission#3/JS/main.js
+++ Assigment_submission#3/JS/main.js
@@ -1,25 +1,25 @@
 
     function m1 () {
-    window.alert(Math.random());
-    var p1 = 5;
-    var p2 = 6;
-    var p3 = 5;
-    var p4 = 2;
-    var t1 = p1 + p2;
-    var t2 = p3 - p4;
-    var d = 48 / 6;
-    var m = 5 * 8;
-    var mo = 25 % 6
-    var x = 5; 
-    x++;
-    var y = 9; 
-    y--;
+    window.alert(Math.random());                    //Display a window alert with random numbers
+    var p1 = 5;          // declared a variable   
+    var p2 = 6;          // declared a variable 
+    var p3 = 5;         // declared a variable 
+    var p4 = 2;         // declared a variable 
+    var t1 = p1 + p2;   // declared t1 to add value
+    var t2 = p3 - p4;   // declared t2 to subtract value
+    var d = 48 / 6;     // variable d to divide value
+    var m = 5 * 8;      //variable m to multiply value
+    var mo = 25 % 6     //variable mo to get the remainder of the value
+    var x = 5;          //declared x as value of 5
+    x++;                //operator for increment
+    var y = 9;          //declared y as value of 9
+    y--;                //operator for decrement
 
 
 
 
-    document.getElementById("Math").innerHTML = "5 + 6 = " + t1 
-    + "<br>" + "5 - 2 =" + t2
+    document.getElementById("Math").innerHTML = "5 + 6 = " + t1    //putting the value
+    + "<br>" + "5 - 2 =" + t2                                      //of the results into HTML element with "Math" id
     + "<br>" + "48 / 6 =" + d 
     + "<br>" + "5 x 8 = " + m
     + "<br>" + "When you divide 25 by 6 you have a remainder of " + mo

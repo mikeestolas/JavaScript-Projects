4663bd67a20b17ac0dcf300c277f97a3d79d3a07
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-05-23 15:14:19 -0400

    .

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/Assignment#32/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/Assignment#32/JS/main.js
@@ -2,4 +2,9 @@
 
 var x = 5;
 
+
 document.getElementById("m1").innerHTML = (x === 5);
+
+
+
+document.getElementById("m2").innerHTML = (x === "5");
---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/Assignment#34/JS/main.js
@@ -0,0 +1,17 @@
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10);
+    document.getElementById("Not2").innerHTML = !(5 > 10);
+}
+
+
+
+
+
+
+    
+    
+    
+    
+
+
+

09c1a344905c2ef3955538f6d6edbdcf57f7fcbc
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-05-24 10:34:58 -0400

    .

--- Basic JavaScript Projects/Project7_scope_time_function/Assignment_Submission#7/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/Assignment_Submission#7/JS/main.js
@@ -6,13 +6,14 @@ function f1() {
   var a = document.getElementById("name").value;//local variable
     
     if (time < 10) {
-      console.log(say = "Good morning");
+      console.log(say = "Good morning");//console.log
     } else if (time < 20) {
-      console.log(say = "Good day");
+      console.log(say = "Good day");//console.log
     } else {
-      console.log(say = "Good evening");
+      console.log(say = "Good evening");//console.log
     }
-    document.getElementById("m1").innerHTML = say + " " + a + " Its nice to see you!";
+    document.getElementById("m1").innerHTML = say + " " 
+    + a + " Its nice to see you!"; //display
   }
     
 

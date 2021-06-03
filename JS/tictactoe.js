dca70a1af2faa534fe6f5a6bbb03c73a27c7002d
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-06-02 20:29:42 -0400

    .

---
+++ JS/tictactoe.js
@@ -0,0 +1,71 @@
+//This variable keeps track of who's turn it is.
+let activePlayer = 'X';
+//This array stores an array of moves. We use this to determine win conditions.
+let selectedSquares = [];
+
+//This function is for placing an x or o in a square.
+function placeXOro(squareNumber) {
+    //This condition ensures a square hasn't be selected already.
+    //The .some() method is used to check each element of selectedSquare array to
+    //see if it contains the square number clicked on.
+    if (!selectedSquares.some(element => element.includes(squareNumber))) {
+        //This variable retrieves the html element id that was clicked.
+        let select = document.getElementById(squareNumber);
+        //This condition checks who's turn it is.
+        if (activePlayer === 'X') {
+            //If activePlayer is equal to 'X', the x.png is placed in HTML.
+            select.style.backgroundImage = 'url("./images/x.png")';
+            //Active player may only be 'X' of 'O' so, if not 'X' it must be 'O'
+         } else {
+             //If activePlayer is equal to 'O', the o.png is placed in HTML.
+             select.style.backgroundImage = 'url("./images/o.png")';         
+         }
+         //squareNumber and activePlayer are concatenated together and added to array.
+         selectedSquares.push(squareNumber + activePlayer);
+         //THis calls a function to check for any win conditions.
+         checkWinConditions();
+         //This condition is for changing the active player.
+         if (activePlayer === 'X') {
+             //If active player is 'X' change it to 'O'.
+             activePlayer = 'O';
+        //If active player is anything other than 'X'.
+         } else {
+             //Change the activePlayer tp 'X'
+             activePlayer = 'X';
+         }
+
+         //This function plays placement sound.
+         Audio('./media/place.mp3');
+         //This condition checks to see if it is computers turn.
+         if(activePlayer ==='O') {
+             //This function disables clicking for computer choice.
+             disableClick();
+             //This function waits 1 second vefore placing the image
+             //and enabling click.
+             setTimeout(function () {computerTurn();}, 1000);
+         }
+         //REturning true is needed for our computerTurn() function to work.
+         return true;
+    }
+
+    //This function results in a random square being selected.
+    function computersTurn() {
+        //This boolean is need for our While loop.
+        let success = false;
+        //This variable stores a random number 0-8
+        let pickASquare;
+        //This condition allows our While loop to keep
+        //trying if a square is selected already.
+        while(!success) {
+            //A random number between 0 and 8 is selected
+            pickASquare = String(Math.floor(Math.random() * 9));
+            //If the random number evaluates returns true, the square hasn't been selected yet.
+            if (placeXOro(pickASquare)) {
+                //This line calls the function.
+                placeXOro(pickASquare);
+                //This changes our boolean and ends the loop.
+                success =true;
+            };    
+        }
+    }
+}
